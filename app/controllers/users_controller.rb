87ec03555965b7cc30e329eff29a5df31f95d43b
KamilSiwek <kontokamilsiwek@gmail.com>
2017-12-13 19:09:30 +0100

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,21 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
   end
+
+  # def index
+  #   @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+  # end
+  #
+  # def show
+  #   @user = User.find(params[:id])
+  #   redirect_to root_url and return unless FILL_IN
+  # end
 
   def new
     @user = User.new
